2071e582db6df3e82387469c607ba5011b08e15e
archipelagozzz <163554669+archipelagozzz@users.noreply.github.com>
2026-02-22 15:37:17 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -36,7 +36,6 @@ local function get_path_as_string(object)
     repeat
         table.insert(_return, 1, current_object.Name)
         current_object = current_object.Parent
-        task.wait()
     until not current_object or not current_object.Parent
     
     return _return
@@ -51,32 +50,34 @@ function find_descendant_of_class(obj, classes_name)
 end
 
 function save_script(sc, _folder_path)
-    if not sc:IsA("ModuleScript") and not sc:IsA("LocalScript") then 
-        if sc:IsA("Folder") then return end
-        writefile(`{_folder_path}/{remove_symbols(`{sc.Name} {sc.ClassName}`)}`, "")
-        return
-    end
-    local start = os.clock()
-    local success_process, tries, _decompiled = safe_decompile(sc)
-    
-    local _path = table.concat(get_path_as_string(sc), ".")
-    local _name = remove_symbols(`{tries or "FAILED"} {sc.Name} {sc.ClassName}.txt`)
+    if sc:IsA("ModuleScript") or sc:IsA("LocalScript") then 
+		local start = os.clock()
+		local success_process, tries, _decompiled = safe_decompile(sc)
+		
+		local _path = table.concat(get_path_as_string(sc), ".")
+		local _name = remove_symbols(`{tries or "FAILED"} {sc.Name} {sc.ClassName}.txt`)
 
-    if getgenv()._extract_debug then 
-        print("------------ SNATCHING -----------------")
-        print(`NAME : {_name}`)
-        print(`PATH : {_path}`)
-        print("------------ SNATCHING -----------------")
-    end
-    
-    local state, _ = pcall(function()
-        return writefile(`{_folder_path}/{_name}`, _decompiled)
-    end)
+		if getgenv()._extract_debug then 
+			print("------------ SNATCHING -----------------")
+			print(`NAME : {_name}`)
+			print(`PATH : {_path}`)
+			print("------------ SNATCHING -----------------")
+		end
+		
+		local state, _ = pcall(function()
+			return writefile(`{_folder_path}/{_name}`, _decompiled)
+		end)
 
-    if state and getgenv()._extract_debug then
-        print(`snatched {sc.Name} at {os.clock()-start}s in {tries} tries`)
-    elseif getgenv()._extract_debug then
-        print(`{sc.Name} ranaway cuh`)
+		if state and getgenv()._extract_debug then
+			print(`snatched {sc.Name} at {os.clock()-start}s in {tries} tries`)
+		elseif getgenv()._extract_debug then
+			print(`{sc.Name} ranaway cuh`)
+		end
+	else
+		local _path = table.concat(get_path_as_string(sc), ".")
+		local _name = remove_symbols(`[{sc.ClassName}] {sc.Name}`)
+
+		writefile(`{_folder_path}/{_name}`, "")
     end
 end
 
@@ -87,17 +88,18 @@ function get_path_by_origin_container(origin_container, object)
         table.insert(_path, 1, current_object)
         if current_object == origin_container or not current_object.Parent then break end
         current_object = current_object.Parent
-        task.wait()
     until not current_object.Parent
 
     return _path
 end
 
+local origin_container
 function save_container_scripts(container, descendant, origin_container)
     if getgenv()._extract_stop then print("Snatching stopped") end
     if not container then warn("Snatching target has to be referenced.") return end
-    origin_container = origin_container or container
-    
+
+	origin_container = origin_container or container
+
     local _array_path = get_path_by_origin_container(origin_container, container, origin_container)
     local _string_array_path = {}
     for index, value in _array_path do
@@ -108,37 +110,40 @@ function save_container_scripts(container, descendant, origin_container)
     local state, err = pcall(function()
         return makefolder(_path)
     end)
-    if not getgenv()._extract_debug then 
-        print(_path)
-        print(state, err)
-    end
 
     local banned_ancestors = getgenv()._extract_blacklist_ancestor or {}
     local classes_only = getgenv()._extract_only_class or {}
+
+	local stamp = os.clock()
+
     for _, obj in container:GetChildren() do
         if getgenv()._extract_stop then break end
         local state_break = false
 
         for _, banned_ancestor in banned_ancestors do
-            if not obj:FindFirstAncestor(banned_ancestor) then -- obj:IsA("LocalScript") or obj:IsA("ModuleScript"))
-                if #classes_only > 0 and not table.find(classes_only, obj.ClassName) then continue end
-                save_script(obj, _path)
-                task.wait()
-            else
+            if obj:FindFirstAncestor(banned_ancestor) then -- obj:IsA("LocalScript") or obj:IsA("ModuleScript"))
                 state_break = true
             end
         end
-   
-        if not state_break and descendant then -- 
-            if #classes_only > 0 and (not find_descendant_of_class(obj, getgenv()._extract_only_class) or #obj:GetChildren() <= 0) then continue end
-            save_container_scripts(obj, getgenv()._extract_descendants, origin_container)
+
+        if not state_break then -- 
+			if #classes_only > 0 and not table.find(classes_only, obj.ClassName) then continue end
+
+            save_script(obj, _path)
+			
+            if descendant and #obj:GetChildren() > 0 then
+				if #classes_only > 0 and (not find_descendant_of_class(obj, getgenv()._extract_only_class)) then continue end
+            	save_container_scripts(obj, getgenv()._extract_descendants)
+			end
         end
     end
 
-    if container ~= origin_cointainer then return end
-    print("----- SNATCHED TARGET -----")
-    print(`done snatched ts target cuh, they from da {origin_container.Parent.Name} and they is {origin_container.Name}`)
-    print("----- SNATCHED TARGET -----")
+	if origin_container.Name == container.Name and origin_container.ClassName == container.ClassName and #origin_container:GetChildren() == #container:GetChildren() then 
+		print("----- SNATCHED TARGET -----")
+    	print(`done snatched ts target cuh, they from da {origin_container.Parent.Name} and they is {origin_container.Name}`)
+		print(`ts lowkey taking me some {os.clock() - stamp}s`)
+    	print("----- SNATCHED TARGET -----")
+	end
 end
 
 local _path = getgenv()._extract_path
